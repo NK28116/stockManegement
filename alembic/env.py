d956a737b3db5cf8687e908316ae63cb5fdb34f5
NK28116 <shoutarou.phillip.w@gmail.com>
2025-12-16 16:52:49 +0900

    env

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,18 @@
+import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+# プロジェクトルートをパスに追加してモジュールをインポート可能にする
+# このファイル(env.py)を格納する `alembic` ディレクトリの親、つまりプロジェクトルートをパスに追加します
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from python.db.database import DATABASE_URL  # noqa: E402
+from python.db.models import Base  # noqa: E402
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +23,12 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# 環境変数等のURLで上書き (alembic.iniのsqlalchemy.urlは無視または空でOK)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
